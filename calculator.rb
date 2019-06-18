478f18c84466e8d29064d27300f3f4ebb87dde9a
Christopher Dent <christopherdent01@gmail.com>
2019-06-18 02:23:41 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,5 +3,3 @@ first_number = 2
 second_number = 5
 sum = (first_number + second_number)
 difference = (first number - second_number)
-product = (first number * second number)
-quotient = (first number / second number)
